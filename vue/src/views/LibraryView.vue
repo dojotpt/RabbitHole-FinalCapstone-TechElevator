34aa55b95355744856a9f905235cc4a77a71e250
don johnson <donjohnsontpt@gmail.com>
2024-08-05 14:45:29 -0400

    push before creating staging branch

--- vue/src/views/LibraryView.vue
+++ vue/src/views/LibraryView.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
+    <div class="library-container">
+    <div id="my-library">
+        <h1>Library View</h1>
         <my-library />
     </div>
+</div>
 </template>
 
 <script>
@@ -14,5 +17,17 @@
 </script>
 
 <style scoped>
-
+.library-container {
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(94, 65, 36, 0.9);
+    border-radius: 10px;
+    width: 80%;
+    height: 80%;
+    align-items:center;
+    border: black 2px solid;
+    margin: auto; 
+    justify-content: center;
+    
+}
 </style>
